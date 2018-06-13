fdd2635ed4d6d052a6dcf41075d9c4a79b63ee83
Amadeusz Sadowski <amis.tau.92@gmail.com>
2018-06-13 08:42:47 +0200

    Release-mode build script

--- scripts/appveyor-ci-build-script-v1.0.ps1
+++ scripts/appveyor-ci-build-script-v1.0.ps1
@@ -10,20 +10,37 @@ $index_url_part = "$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG"
 echo "Branch: $branch, is PR: $pr"
 
 # standalone BSR
-wham publish bsr -v Verbose -bsr-filename standalone
+wham publish bsr -v Verbose -bsr-filename snapshot
 
 # latest only from non-PR
 
 if (-not $pr) {
 
-  echo "publishing artifacts for $branch branch-feed - links currently not supported by BattleScribe";
+  echo "publishing artifacts for $branch branch-feed - links currently not supported by BattleScribe"
+  $filename_core = "$slug-branch-latest"
   
   wham publish bsr,bsi `
   -v Verbose `
-  -i index-latest `
-  -url "https://ci.appveyor.com/api/projects/$index_url_part/artifacts/artifacts/index-latest.bsi?branch=$branch&pr=false" `
+  -i $filename_core `
+  -url "https://ci.appveyor.com/api/projects/$index_url_part/artifacts/artifacts/$filename_core.bsi?branch=$branch&pr=false" `
   -no-index-datafiles `
-  -bsr-filename latest `
-  -additional-urls "https://ci.appveyor.com/api/projects/$index_url_part/artifacts/artifacts/latest.bsr?branch=$branch&pr=false"
+  -bsr-filename $filename_core `
+  -additional-urls "https://ci.appveyor.com/api/projects/$index_url_part/artifacts/artifacts/$filename_core.bsr?branch=$branch&pr=false"
+  
+}
+
+# for release tags
+
+if ($env:APPVEYOR_REPO_TAG) {
+
+  echo "Publishing artifacts for $tag release feed"
+  
+  wham publish bsr,bsi `
+  -v Verbose `
+  -i $slug `
+  -url "https://github.com/$index_url_part/releases/download/$tag/$slug.bsi" `
+  -no-index-datafiles `
+  -bsr-filename $slug `
+  -additional-urls "https://github.com/$index_url_part/releases/download/$tag/$slug.bsr"
   
 }
