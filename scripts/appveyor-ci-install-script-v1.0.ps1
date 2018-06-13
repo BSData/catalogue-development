91c5d2187380a30063702005172540695b8516cd
Amadeusz Sadowski <amis.tau.92@gmail.com>
2018-06-13 21:39:59 +0200

    Package install script into function

--- scripts/appveyor-ci-install-script-v1.0.ps1
+++ scripts/appveyor-ci-install-script-v1.0.ps1
@@ -1,9 +1,14 @@
 # This is the pre-build script for AppVeyor CI builds
-# It produces artifacts for use in BattleScribe
+# It installs required tools
 
-echo "BSData PowerShell 'install' script for AppVeyor CI builds - v1.0"
+function InstallScript {
+
+Write-Host "Executing install script v1.0" -fore Green
 
 # install 'wham' - .NET Core global tool for datafile management
 dotnet tool install -g wham `
---version 0.5.23-alpha `
---add-source https://www.myget.org/F/warhub/api/v3/index.json
+  --version 0.5.23-alpha `
+  --add-source https://www.myget.org/F/warhub/api/v3/index.json
+  
+} # end InstallScript
+InstallScript
