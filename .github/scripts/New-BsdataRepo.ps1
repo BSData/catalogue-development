cc694778b43f7636f0d3b126d70c5195a6910c98
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-09-14 15:55:49 +0200

    fix: Update script to v0.15 of PowerShellForGitHub

--- .github/scripts/New-BsdataRepo.ps1
+++ .github/scripts/New-BsdataRepo.ps1
@@ -20,62 +20,6 @@ param (
     [string] $AccessToken
 )
 
-function CreateRepo {
-    [CmdletBinding(SupportsShouldProcess)] param()
-    # https://developer.github.com/v3/repos/#create-repository-using-a-repository-template
-    $repotemplateAcceptHeader = 'application/vnd.github.baptiste-preview+json'
-    $generateParams = @{
-        Method = 'POST'
-        UriFragment = "/repos/$OwnerName/TemplateDataRepo/generate"
-        AcceptHeader = $repotemplateAcceptHeader
-        Body = @{
-            owner = $OwnerName
-            name = $RepositoryName
-            description = $Description
-        } | ConvertTo-Json
-    }
-    Invoke-GHRestMethod @generateParams @authParams
-}
-function UpdateTopics {
-    [CmdletBinding(SupportsShouldProcess)] param()
-    # https://developer.github.com/v3/repos/#list-all-topics-for-a-repository
-    # https://developer.github.com/v3/repos/#replace-all-topics-for-a-repository
-    $getTopicsResult = Get-GitHubRepositoryTopic @repoParams @authParams
-    $topics = @($getTopicsResult.names, "battlescribe-data") | Where-Object { $_ }
-    Set-GitHubRepositoryTopic -Name $topics @repoParams @authParams
-}
-function UpdateReadme {
-    [CmdletBinding(SupportsShouldProcess)] param()
-    # https://developer.github.com/v3/repos/contents/#get-the-readme
-    $getReadmeParams = @{
-        Method = 'GET'
-        UriFragment = "/repos/$OwnerName/$RepositoryName/readme"
-    }
-    $getReadmeResult = Invoke-GHRestMethod @getReadmeParams @authParams
-    if ($getReadmeResult.encoding -ne 'base64') {
-        Write-Warning "Unknown readme content encoding: $($getReadmeResult.encoding)"
-        return "Unknown encoding"
-    }
-    $readmePath = $getReadmeResult.path
-    $readme = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($getReadmeResult.content))
-    $readmePatched = $readme.Replace('TemplateDataRepo', $RepositoryName).Replace('Template Data Repo', $Description)
-    if ($readme -ceq $readmePatched) {
-        return "Skipped"
-    }
-    $base64Patched = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($readmePatched))
-    # https://developer.github.com/v3/repos/contents/#create-or-update-a-file
-    $setReadmeParams = @{
-        Method = 'PUT'
-        UriFragment = "/repos/$OwnerName/$RepositoryName/contents/$readmePath"
-        Body = @{
-            'message' = "docs: Replace template values in $readmePath"
-            'content' = $base64Patched
-            'sha' = $getReadmeResult.sha
-        } | ConvertTo-Json
-    }
-    Invoke-GHRestMethod @setReadmeParams @authParams
-}
-
 Write-Verbose "Processing parameters"
 # defaulting description from name
 if ([string]::IsNullOrWhiteSpace($Description))
@@ -92,26 +36,62 @@ $authParams = @{
     AccessToken = $AccessToken
 }
 $repoParams = @{
-    OwnerName = 'BSData'
+    OwnerName = $OwnerName
     RepositoryName = $RepositoryName
 }
 $homepage = "http://battlescribedata.appspot.com/#/repo/$RepositoryName"
 
 Write-Verbose "Starting operations."
 $result = @{}
-$result['CreateRepo'] = CreateRepo
-Write-Verbose "Repo created at $($result.CreateRepo.html_url)"
 
-$result['SecureRepo'] = & "$PSScriptRoot/Set-BsdataRepoSecurity.ps1" @repoParams @authParams
+$newRepoParams = @{
+    OwnerName = $OwnerName
+    RepositoryName = "TemplateDataRepo"
+    TargetOwnerName = $OwnerName
+    TargetRepositoryName = $RepositoryName
+    Description = $Description
+}
+$repo = New-GitHubRepositoryFromTemplate @newRepoParams @authParams
+$result['CreateRepo'] = $repo
+Write-Verbose "Repo created at $($repo.html_url)"
+
+$result['SecureRepo'] = $repo | New-GitHubRepositoryBranchProtectionRule -BranchName $repo.default_branch -EnforceAdmins @authParams
 Write-Verbose "Security rules applied"
 
 $result['UpdateHomepage'] = Update-GitHubRepository @repoParams @authParams -Homepage $homepage
 Write-Verbose "Homepage updated to $homepage"
 
-$result['UpdateTopics'] = UpdateTopics
-Write-Verbose "Topics set to $($result.UpdateTopics.names)"
 
-$result['UpdateReadme'] = UpdateReadme
+$topicsResponse = Get-GitHubRepositoryTopic @repoParams @authParams
+$topics = @($topicsResponse.names, "battlescribe-data") | Where-Object { $_ }
+    | Set-GitHubRepositoryTopic @repoParams @authParams -PassThru
+$result['UpdateTopics'] = $topics
+Write-Verbose "Topics set to $($topics.names)"
+
+$result['UpdateReadme'] = . {
+    # https://developer.github.com/v3/repos/contents/#get-the-readme
+    $getReadmeParams = @{
+        Method = 'GET'
+        UriFragment = "/repos/$OwnerName/$RepositoryName/readme"
+    }
+    $getReadmeResult = Invoke-GHRestMethod @getReadmeParams @authParams
+    if ($getReadmeResult.encoding -ne 'base64') {
+        Write-Warning "Unknown readme content encoding: $($getReadmeResult.encoding)"
+        return "Unknown encoding"
+    }
+    $readme = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($getReadmeResult.content))
+    $readmePatched = $readme.Replace('TemplateDataRepo', $RepositoryName).Replace('Template Data Repo', $Description)
+    if ($readme -ceq $readmePatched) {
+        return "Skipped"
+    }
+    $updateReadmeParams = @{
+        Path = $getReadmeResult.path
+        CommitMessage = "docs: Replace template values in README"
+        Content = $readmePatched
+        Sha = $getReadmeResult.sha
+    }
+    $repo | Set-GitHubContent @updateReadmeParams @authParams -PassThru
+}
 Write-Verbose "Readme updated"
 
 if (-not $KeepCreator -and $env:GITHUB_ACTOR)
@@ -121,10 +101,10 @@ if (-not $KeepCreator -and $env:GITHUB_ACTOR)
     $result["RemoveCreator"] = Invoke-GHRestMethod -UriFragment $uri -Method Delete @authParams
     Write-Verbose "Collaborator removed: $login"
 }
-$Collaborators |
-    ForEach-Object { $_ -replace "^@" } |
-    Where-Object { -not [string]::IsNullOrWhiteSpace($_) } |
-    ForEach-Object {
+$Collaborators
+    | ForEach-Object { $_ -replace "^@" }
+    | Where-Object { -not [string]::IsNullOrWhiteSpace($_) }
+    | ForEach-Object {
         $login = $_
         $uri = "/repos/$OwnerName/$RepositoryName/collaborators/$login"
         $result["Invite-$login"] = Invoke-GHRestMethod -UriFragment $uri -Method Put @authParams
